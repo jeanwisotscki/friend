69acf0beae948221200b8d185d62e21e0c3c5734
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-12-07 16:50:59 -0300

    refactor: testando estilos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,14 +7,14 @@ import * as Comp from "../components";
 export default function Home() {
   return (
     <Comp.LayoutBase>
-      <Atom.GlitchWrapper>
+      <Atom.MainTextWrapper>
+        <Atom.MainText data-text="hugo defante">hugo defante</Atom.MainText>
         <div>
-          <Atom.MainText>hugo</Atom.MainText>
-          <Atom.MainText>defante</Atom.MainText>
+          <p>&copy; 2022 U.T.I DO ROCK</p>
         </div>
-      </Atom.GlitchWrapper>
+      </Atom.MainTextWrapper>
       <Atom.LinkWrapper>
-        <Link href={"/menu"}>Vasculhar</Link>
+        <Link href={"/menu"}>press start button</Link>
       </Atom.LinkWrapper>
     </Comp.LayoutBase>
   );
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -1,11 +1,19 @@
 import styled from "styled-components";
 
-export const GlitchWrapper = styled.div`
-  height: 85vh;
+export const MainTextWrapper = styled.div`
+  height: 75vh;
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
   text-align: center;
+
+  div {
+    width: 100%;
+    margin-top: 2rem;
+    color: white;
+    text-align: right;
+  }
 `;
 
 export const MainText = styled.p`
@@ -22,31 +30,64 @@ export const MainText = styled.p`
     rgba(0, 194, 255, 1) 54%,
     rgba(238, 238, 238, 1) 87%
   );
-
   background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 
+  &::before {
+    content: attr(data-text);
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: #fff;
+    font-size: 5rem;
+    text-shadow: -5px -5px;
+  }
+
   @media screen and (max-width: 768px) {
     font-size: 4rem;
+
+    &::before {
+      font-size: 4rem;
+    }
   }
   @media screen and (max-width: 444px) {
     font-size: 2.5rem;
+
+    &::before {
+      font-size: 2.5rem;
+    }
   }
   @media screen and (max-width: 297px) {
     font-size: 2rem;
+
+    &::before {
+      font-size: 2rem;
+    }
   }
 `;
 
 export const LinkWrapper = styled.div`
-  text-align: right;
+  text-align: center;
+  text-transform: uppercase;
 
   a {
     color: ${(props) => props.theme.colors.primary};
+    padding: 1rem;
     transition: 0.2s;
+    position: relative;
 
     &:hover {
       color: ${(props) => props.theme.colors.yellow};
+
+      &::before {
+        content: ">";
+        width: 10px;
+        height: 10px;
+        position: absolute;
+        left: -10px;
+        font-weight: bold;
+      }
     }
   }
 
