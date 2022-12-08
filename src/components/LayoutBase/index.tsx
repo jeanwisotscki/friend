c0b62891b9f316bdf8416a40a74e9fecb2d91b65
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-12-08 11:10:12 -0300

    refactor: melhorias no responsivo

--- src/components/LayoutBase/index.tsx
+++ src/components/LayoutBase/index.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 
+import breakpoints from "../../common/breakpoints";
+
 const Container = styled.main`
+  width: 90%;
   max-width: 50rem;
-  width: 80%;
+  min-width: 279px;
+  min-height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: 1px solid yellow;
+
   margin: 0 auto;
+
+  @media screen and (${breakpoints.tablet}) {
+    & {
+      width: 100%;
+      padding: 1rem;
+    }
+  }
 `;
 
 interface ILayoutBaseProps {
