0048e27362e7104f483e3aa232ccd1ef840c8db0
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-12-13 08:57:30 -0300

    new: página: músicas

--- src/pages/musicas/index.tsx
+++ src/pages/musicas/index.tsx
@@ -2,35 +2,38 @@ import React from "react";
 
 import * as Atom from "./styles";
 
+import * as Comp from "../../components";
+
 import musicas from "../../data/musicas.json";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Musicas() {
   return (
-    <Atom.Container>
-      <h2>Musicas</h2>
+    <Comp.LayoutBase>
+      <Atom.Container>
+        <h2>Musicas</h2>
 
-      <div>
-        {musicas.map((item, index: number) => (
-          <div key={index}>
-            <br />
-            <br />
-            <h3>{item.song}</h3>
-            <p>{item.album}</p>
-            <p>{item.author}</p>
-            <Image
-              width={100}
-              height={100}
-              src={item.imageHref}
-              alt=" capa do album"
-            />
-            <a href={item.songHref}>compre agora</a>
-
-            <br />
-            <br />
-          </div>
-        ))}
-      </div>
-    </Atom.Container>
+        <Atom.SongsWrap>
+          {musicas.map((item, index: number) => (
+            <Atom.SongCard key={index}>
+              <Image
+                width={200}
+                height={200}
+                src={item.imageHref}
+                alt=" capa do album"
+              />
+              <div>
+                <h3>{item.song}</h3>
+                <p>{item.album}</p>
+                <p>{item.author}</p>
+                <a href={item.songHref}>compre agora</a>
+              </div>
+            </Atom.SongCard>
+          ))}
+        </Atom.SongsWrap>
+        <Link href={"/menu"}>Voltar</Link>
+      </Atom.Container>
+    </Comp.LayoutBase>
   );
 }
--- src/pages/musicas/styles.ts
+++ src/pages/musicas/styles.ts
@@ -3,5 +3,45 @@ import styled from "styled-components";
 export const Container = styled.div`
   height: 80vh;
   color: #fff;
-  overflow: scroll;
+`;
+
+export const SongsWrap = styled.div`
+  height: 80vh;
+  padding: 1rem;
+  overflow-y: scroll;
+  scrollbar-width: thin;
+
+  &::-webkit-scrollbar {
+    width: 12px; /* width of the entire scrollbar */
+  }
+
+  &::-webkit-scrollbar-track {
+    background: orange; /* color of the tracking area */
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: blue; /* color of the scroll thumb */
+    border-radius: 20px; /* roundness of the scroll thumb */
+    border: 3px solid orange; /* creates padding around scroll thumb */
+  }
+`;
+
+export const SongCard = styled.div`
+  border: double;
+  margin: 1rem 0;
+  display: flex;
+  gap: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  div {
+    padding: 2rem;
+
+    a {
+      background-color: red;
+      position: absolute;
+      bottom: 20px;
+      right: 20px;
+    }
+  }
 `;
