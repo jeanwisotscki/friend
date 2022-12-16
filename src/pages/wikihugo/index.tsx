b8b46a7eeca4c906c90755ffe5eb6a7f168c8e98
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-12-16 11:27:49 -0300

    refactor: adiconado conteúdo da seção discografia

--- src/pages/wikihugo/index.tsx
+++ src/pages/wikihugo/index.tsx
@@ -128,43 +128,41 @@ export default function WikiHugo() {
             </p>
 
             {/**********************************************/}
-
-            <h2>Discografia</h2>
-            <hr />
-            <p>
-              <strong>Hugo Defante</strong>, nome artístico de{" "}
-              <strong>Hugo Defante Rodrigues</strong>{" "}
-              <i> (Rio de Janeiro, 26 de maio de 1987)</i> é um virtuoso
-              guitarrista carioca, considerado na sua região, um dos melhores de
-              seu estilo.
-            </p>
-
-            <p>
-              Influenciado pelos metais pesados de <strong>Slayer</strong> e os
-              teclados sintetizados de <strong>Iron Maiden</strong>, Hugo é
-              conhecido na sua região por ser um dos precursores do chamado{" "}
-              <strong>Synth metal</strong>, que envolve heavy metal com
-              elementos de música eletrônica. Junto ao seu irmão{" "}
-              <strong>Diogo Defante</strong>, também tornou-se um dos pioneiros
-              do <strong>Robson metal</strong>, com a temática humorística de
-              suas letras e com os sons dúvidosos do seu berrante criando a
-              atmosférica bem-humorada das canções. Seguindo estes estilos, ele
-              ajudou a popularizar o <strong>berrante</strong>{" "}
-              <i>
-                (espécie de buzina usada desde a antiguidade por pastores de
-                ovelhas e gados)
-              </i>{" "}
-              dentro do heavy metal em geral e veio a ser o primeiro guitarrista
-              a tocar esse instrumento em um show para milhares de pessoas. Hugo
-              também já foi considerado o guitarrista mais rápido do Velho
-              Oeste.
-            </p>
-            <p>
-              <strong>Hugo Defante</strong> é, além de guitarrista e
-              multi-instrumentista, compositor, streamer e pixel artist.
-              Guitarrista á mais de 20 anos, Hugo é com toda certeza um dos
-              guitarristas do Brasil!
-            </p>
+            <section>
+              <h2>Discografia</h2>
+              <hr />
+              <br />
+              <div>
+                <strong>Aquilonia </strong>
+                <i>(10 de Dezembro de 2019)</i>
+                <br />
+                <br />
+                <ul>
+                  <li>
+                    <i>* Aquilonia</i>
+                  </li>
+                </ul>
+              </div>
+              <br />
+              <br />
+              <div>
+                <strong>Electron </strong>
+                <i>(10 de Outubro de 2020)</i>
+                <br />
+                <br />
+                <ul>
+                  <li>
+                    <i>* Showtime</i>
+                  </li>
+                  <li>
+                    <i>* Electron</i>
+                  </li>
+                  <li>
+                    <i>* Message From The Past</i>
+                  </li>
+                </ul>
+              </div>
+            </section>
           </Atom.TextContainer>
         </Atom.MainContentWrapper>
       </Atom.Container>
