40779aa2a529e8c3b7f72c774577a1f7679d3315
Jean Wisotscki <jean1wisotscki@gmail.com>
2023-01-04 11:37:00 -0300

    added: favicon adicionado

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -49,7 +49,7 @@ export default class MyDocument extends Document {
           />
 
           {/* favicon */}
-          <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 
           {/* fonts */}
           {/* Press Start 2P */}
